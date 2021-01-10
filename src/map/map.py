8f5db3083a553f8624fd085b6ef415210ec48dc5
James <james.rosstwo@gmail.com>
2021-01-10 12:09:58 -0800

    finalize recommender

--- src/map/map.py
+++ src/map/map.py
@@ -46,6 +46,7 @@ class CovidMap:
         self.client = googlemaps.Client(key=self.api_key)
         self.graph = None
         self.exposures = []
+        self.current_city = "Vancouver"
 
         str(ROOT_PATH / self.settings["save_path"])
         # # Look up an address with reverse geocoding
@@ -65,11 +66,11 @@ class CovidMap:
         if transport_mode is TransportMode.DRIVE:
             self.graph = ox.graph_from_point((loc_lat, loc_long), network_type='drive', dist=5000)
         elif transport_mode is TransportMode.TRANSIT:
-            pass
+            self.graph = ox.graph_from_point((loc_lat, loc_long), network_type='drive', dist=5000)
         elif transport_mode is TransportMode.BIKE:
-            self.graph = ox.graph_from_point((loc_lat, loc_long), network_type='bike')
+            self.graph = ox.graph_from_point((loc_lat, loc_long), network_type='bike', dist=5000)
         elif transport_mode is TransportMode.WALK:
-            self.graph = ox.graph_from_point((loc_lat, loc_long), network_type='walk')
+            self.graph = ox.graph_from_point((loc_lat, loc_long), network_type='walk', dist=5000)
         else:
             raise Exception("Invalid transport mode passed to plot_route")
 
@@ -85,11 +86,11 @@ class CovidMap:
                              max_width=width)
         return popup
 
-    def map_events(self, map_obj):
+    def map_exposures(self, map_obj):
         events = get_events()
         marker_cluster = MarkerCluster().add_to(map_obj)
         for event in events:
-            geocoded_loc = self.client.geocode(event["address"])
+            geocoded_loc = self.client.geocode(event["address"] + ", " + self.current_city)
             if len(geocoded_loc) == 0:
                 continue
             geocoded_loc = geocoded_loc[0]["geometry"]["location"]
@@ -110,6 +111,32 @@ class CovidMap:
         polyline_path = self.decode_path(route)
         return folium.PolyLine(polyline_path, color=color, width=8)
 
+    def get_types(self, location):
+        filter_list = ["establishment", "point_of_interest", "food", "store"]
+        return [x for x in location["types"] if x not in filter_list]
+
+    def get_similar_locs(self, location, loc_query, max_num=10):
+        types = self.get_types(location)
+        return self.client.geocode(types[0] + " near " + loc_query + self.current_city)[:max_num]
+
+    def waypoint_lower_risk_locs(self, map, current_risk, location, loc_query):
+        locs = self.get_similar_locs(location, loc_query)
+        for loc in locs:
+            lat, lng = loc["geometry"]["location"]["lat"], loc["geometry"]["location"]["lng"]
+            loc_risk = calculate_risk_score(self, lat, lng)
+            if loc_risk > current_risk:
+                continue
+            marker_html = "<strong> Alternate Location </strong> <br> <strong> Risk score: </strong>" + str(
+                loc_risk * 100)[:4] + "% <br>" + loc["formatted_address"]
+            folium.Marker(
+                [lat, lng],
+                popup=self.make_popup(marker_html),
+                icon=folium.Icon(icon="plus-circle",
+                                 angle=0,
+                                 prefix="fa",
+                                 tooltip="Alternate Location", color="green")
+            ).add_to(map)
+
     def plot_route(self, dest, orig=None):
         if orig is None:
             orig = self.home_loc
@@ -117,11 +144,9 @@ class CovidMap:
 
         origin = self.client.geocode(orig)[0]
         orig_loc = (origin["geometry"]["location"]["lat"], origin["geometry"]["location"]["lng"])
-        orig_node = ox.get_nearest_node(self.graph, orig_loc)
 
         destination = self.client.geocode(dest)[0]
         dest_loc = (destination["geometry"]["location"]["lat"], destination["geometry"]["location"]["lng"])
-        dest_node = ox.get_nearest_node(self.graph, dest_loc)
 
         route = self.get_gmaps_path(orig_loc, dest_loc)
 
@@ -129,17 +154,21 @@ class CovidMap:
 
         self.make_polyline(route).add_to(route_map)
 
-        self.map_events(route_map)
+        self.map_exposures(route_map)
 
         folium.Marker(
             list(orig_loc), popup=self.make_popup("<i>" + orig + "</i>"), tooltip="Origin"
         ).add_to(route_map)
 
         dest_risk_score = calculate_risk_score(self, *dest_loc)
+
+        if dest_risk_score > 0.5:
+            self.waypoint_lower_risk_locs(route_map, dest_risk_score, destination, dest)
+
         destination_html = "<strong> Location: </strong><i>" + dest + \
                            "</i> <br> <strong> Risk Score: " + str(dest_risk_score * 100)[:4] + "%" \
                                                                                                 "</strong> <br> <strong> Recommendation: </strong>" + \
-                           give_recommendation(destination, dest_risk_score)
+                           give_recommendation(destination, dest, dest_risk_score)
         folium.Marker(
             list(dest_loc), popup=self.make_popup(destination_html), tooltip="Destination"
         ).add_to(route_map)
--- src/map/recommender/recommender.py
+++ src/map/recommender/recommender.py
@@ -1,9 +1,36 @@
-def best_time(location):
-    pass
+from datetime import datetime
+
+import requests
+from src.definitions import SETTINGS, ROOT_PATH
+import json
 
 
-def give_recommendation(location, risk_score):
-    safest_time = "Xpm"
+def best_time(location, query):
+    url = "https://besttime.app/api/v1/keys/" + SETTINGS["map"]["best_time_api_key"]
+
+    payload = {}
+    headers = {}
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    url = "https://besttime.app/api/v1/forecasts"
+
+    params = {
+        'api_key_private': SETTINGS["map"]["best_time_api_key"],
+        'venue_name': query,
+        'venue_address': location["formatted_address"]
+    }
+
+    response = requests.request("POST", url, params=params)
+    data = json.loads(response.text)
+
+    return data["analysis"][datetime.today().weekday()]
+
+
+def give_recommendation(location, query, risk_score):
+    best_time_analysis = best_time(location, query)
+    safest_times = [str(x) + ":00, " for x in best_time_analysis["quiet_hours"][:-1]]
+    safest_time = "".join(safest_times) + "or " + str(best_time_analysis["quiet_hours"][-1]) + ":00"
+
     if risk_score > 0.8:
         return "This is in an area with high risk, it may be a good idea to consider going somewhere else. " \
                "Safer alternatives to this location have been placed on your map with a green marker. " \
@@ -11,7 +38,7 @@ def give_recommendation(location, risk_score):
     if risk_score > 0.5:
         return "This is in an area that has a higher than average risk. " \
                "We recommend going at " + safest_time + ", when it should be the least crowded." \
-               "If you are interested in looking at alternative locations, they have been placed on your map as green markers."
+                                                        "If you are interested in looking at alternative locations, they have been placed on your map as green markers."
     if risk_score > 0.2:
         return "This is in an area with normal risk. " \
                "If you'd like to be safest, we recommend visiting at " + safest_time + ", when it is least busy."
